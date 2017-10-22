2e89f0c9dc2cbb7044216949d2fde4844d6bedc1
RedBeardCory <corypatto@gmail.com>
2017-10-22 19:23:14 +1100

    added default message for commands

--- bot.js
+++ bot.js
@@ -11,6 +11,7 @@ const bot = new Discord.Client({
 bot.on('ready', () => {
   console.log('Logged in as %s - %s\n', bot.username, bot.id);
   bot.setPresence({
+    status: 'online',
     game: {
       name: 'with little cats' // move this to conf later
     }
@@ -18,25 +19,29 @@ bot.on('ready', () => {
 });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
+  // Listen for messages that will start with `!`
+  if (message.substring(0, 1) == '!') {
+    var args = message.substring(1).split(' ');
+    var cmd = args[0];
 
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.simulateTyping( channelID );
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
+    args = args.splice(1);
+    switch(cmd) {
+      // !ping
+      case 'ping':
+        bot.simulateTyping( channelID );
+        bot.sendMessage({
+          to: channelID,
+          message: 'Pong!'
+        });
+      break;
+      default:
+        bot.sendMessage({
+          to: channelID,
+          message: 'This is not the command you are looking for *mystical hand wave*'
+        })
+      break;
+    }
+  }
 });
 
 // need to look at how to check for user joining voice channel
@@ -66,3 +71,19 @@ bot.on('presence', ( user, userID, status, game, event ) => {
     message: msg
   });
 });
+
+// Automatically reconnect if connection drops
+bot.on('disconnect', () => {
+  bot.connect();
+});
+
+// Monitors a voice channel for particular changes
+monitorVoiceChannel = (channelID, serverID) => {
+  var users = bot.servers[serverID].channels[channelID].members;
+  console.log(utill.inspect(users));
+  setInterval(() => {
+    // check to see if there are any changes since last time
+    var nextUsers = bot.servers[serverID].channels[channelID].members;
+    console.log(util.inspect(nextUsers));
+  }, 10000);
+}
